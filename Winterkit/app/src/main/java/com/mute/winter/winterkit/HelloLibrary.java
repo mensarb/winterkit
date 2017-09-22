878ee38f1f7351d773fbdffb26ee3d472769fa13
mensarb <sanak4n@gmail.com>
2017-09-22 09:14:07 +0200

    init

---
+++ Winterkit/app/src/main/java/com/mute/winter/winterkit/HelloLibrary.java
@@ -0,0 +1,15 @@
+package com.mute.winter.winterkit;
+
+import android.util.Log;
+
+/**
+ * @author dkoller
+ * @since 22.09.2017
+ */
+
+public class HelloLibrary {
+
+    public void sayHello(){
+        Log.d("hello", "hello");
+    }
+}
